5b882d4e1a42a2a0c6d0a3acef49cb8cf4a252cb
Mohan Rao Dolla <raomohan2006@yahoo.com>
2019-03-07 14:27:32 +0000

    govuk-assets-styling 3.2.3 added (#6)
    * govuk-assets-styling 3.2.3 added
    
    * play version is changed to 26
    
    * Removed play 2.5 dependencies
    
    * Removed uniform redundant versions

---
+++ public/javascripts/application.js
@@ -0,0 +1,57 @@
+/* global $ */
+/* global jQuery */
+/* global GOVUK */
+
+
+$(document).ready(function () {
+    // Turn off jQuery animation
+    jQuery.fx.off = true;
+
+    // Where .multiple-choice uses the data-target attribute
+    // to toggle hidden content
+    var showHideContentFoo = new GOVUK.ShowHideContentFoo();
+
+    showHideContentFoo.init();
+
+    $(".govuk-radios__item").addClass('multiple-choice');
+
+    $('input.volume').keyup(function(event) {
+
+        // format number
+        $(this).val(function(index, value) {
+            return value
+                .replace(/\D/g, "")
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        });
+    });
+
+
+});
+
+function removeSpecialChars( myid ) {
+    return myid.replace( /(:|\.|\[|\]|,|=|@)/g, "\\$1" );
+}
+
+window.onload = function () {
+
+
+
+
+
+    function gaWithCallback(send, event, category, action, label, callback) {
+        ga(send, event, category, action, label, {
+            hitCallback: gaCallback
+        });
+        var gaCallbackCalled = false;
+        setTimeout(gaCallback, 5000);
+
+        function gaCallback() {
+            if(!gaCallbackCalled) {
+                callback();
+                gaCallbackCalled = true;
+            }
+        }
+    }
+
+
+};
