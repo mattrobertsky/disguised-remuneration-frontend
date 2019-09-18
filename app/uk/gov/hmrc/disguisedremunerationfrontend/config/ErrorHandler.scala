7801c6c71f83ddda2c3a9da215f4b532b96d3cab
Matt Roberts <mattrobertsky@users.noreply.github.com>
2019-09-18 15:44:44 +0100

    500 and 404 handling

--- app/uk/gov/hmrc/disguisedremunerationfrontend/config/ErrorHandler.scala
+++ app/uk/gov/hmrc/disguisedremunerationfrontend/config/ErrorHandler.scala
@@ -17,19 +17,55 @@
 package uk.gov.hmrc.disguisedremunerationfrontend.config
 
 import javax.inject.{Inject, Singleton}
-
-import play.api.i18n.MessagesApi
-import play.api.mvc.{Request,AnyContent}
-import play.twirl.api.{Html,HtmlFormat}
-import uk.gov.hmrc.play.bootstrap.http.FrontendErrorHandler
-import uk.gov.hmrc.disguisedremunerationfrontend.views
 import ltbs.uniform.UniformMessages
+import play.api.i18n
+import play.api.i18n.MessagesApi
+import play.api.mvc.Request
+import play.twirl.api.Html
+import uk.gov.hmrc.disguisedremunerationfrontend.views
+import uk.gov.hmrc.play.bootstrap.http.FrontendErrorHandler
 
 @Singleton
 class ErrorHandler @Inject()(val messagesApi: MessagesApi, implicit val appConfig: AppConfig) extends FrontendErrorHandler {
 
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)(implicit request: Request[_]): Html = {
-    implicit val messages: UniformMessages[Html] = UniformMessages.echo.map(HtmlFormat.escape)
+    implicit val messages: UniformMessages[Html] = convertMessages(messagesApi.preferred(request))
     views.html.error_template(pageTitle, heading, message)
   }
+
+  // this is here temporarily, see https://github.com/ltbs/uniform-scala/issues/73
+  def convertMessages(input: i18n.Messages, escapeHtml: Boolean = false): UniformMessages[Html] = {
+    val stringMessages = new UniformMessages[String]{
+      override def apply(key: List[String],args: Any*): String = {
+        input(key, args:_*)
+      }
+      override def apply(key: String,args: Any*): String = {
+        input(key, args:_*)
+      }
+      def get(key: String,args: Any*): Option[String] = if (input.isDefinedAt(key))
+        Some(input.messages(key, args:_*))
+      else
+        None
+
+      override def get(key: List[String],args: Any*): Option[String] = key collectFirst {
+        case k if input.isDefinedAt(k) => input.messages(k, args:_*)
+      }
+
+      def list(key: String,args: Any*): List[String] = {
+        @annotation.tailrec
+        def inner(cnt: Int = 2, acc: List[String] = Nil): List[String] =
+          get(s"$key.$cnt", args:_*) match {
+            case Some(m) => inner(cnt+1, m :: acc)
+            case None    => acc
+          }
+
+        List(key, s"$key.1").map(get(_, args:_*)).flatten ++ inner().reverse
+      }
+    }
+    if (escapeHtml) stringMessages.map(
+      play.twirl.api.HtmlFormat.escape
+    ) else
+      stringMessages.map(Html.apply)
+  }
+
 }
